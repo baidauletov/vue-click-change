49422ef5bc0ce6d465dec0a9699157048db70102
Sultan <baidauletov.nr@gmail.com>
2020-06-14 23:53:10 +0600

    sass loader

---
+++ src/components/ui/vBtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="button" :style="styles()">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.button {
+  padding: 0.5rem 1rem;
+  border: 1px solid #005caf;
+  border-radius: 2rem;
+}
+</style>
